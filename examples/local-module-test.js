0dc64c81dcd4f59a02ca3ce3e0cdd807c11cadc0
Paul Dixon <paul.dixon@logentries.com>
2014-12-31 11:45:10 +0000

    winston tested, err and log event emit..

--- examples/local-module-test.js
+++ examples/local-module-test.js
@@ -1,9 +1,21 @@
-var http=require('http'), le=require('../lib/logentries.js');
+var http=require('http'), le=require('../lib/logentries.js');//, winston=require('winston');
 
 var leToken='1ed3e842-b5a1-4a8d-9783-0f266e7a6a4d', log=le.logger({token:leToken});
 
+//log.winston(winston,{
+//  transports: [
+//    new winston.transports.Console({ level: 'warn' }),
+//    new winston.transports.File({ filename: 'somefile.log', level:'silly' })
+//  ]
+//});
+
 log.on('error',function(err){
   console.log('LOG ERROR: ', err);
+})
+
+// Why would I want to do this ?
+log.on('log',function(e){
+  console.log('LOG EVENT', e);
 })
 
 http.createServer(function(req, res){
---
+++ examples/winston-test.js
@@ -0,0 +1,22 @@
+var winston=require('winston'), le=require('../lib/logentries.js');
+
+var leToken='1ed3e842-b5a1-4a8d-9783-0f266e7a6a4d', leLog=le.logger({token:leToken});
+
+leLog.winston(winston);
+
+var logger = new winston.Logger({
+  transports: [
+    new winston.transports.LogentriesLogger({ level: 'debug' }),
+    new winston.transports.File({ filename: 'somefile.log', level:'silly' })
+  ]
+});
+
+//logger.transports.console.level('silly');
+
+logger.log('silly','SILLY Howdy..');
+logger.log('debug','DEBUG Howdy..');
+logger.log('verbose','VERBOSE Howdy..');
+logger.log('info','INFO Howdy..');
+logger.log('warn','WARN Howdy..',['big','feckin','deal']);
+logger.log('error','ERROR Howdy %s %s','template','string',{a:1,b:2},function(){ console.log('Why (TF) would I want to do this?') });
+logger.log('Whatever...'); // Nada..?
