829f4074c7fb143165c2ac458261a477f6510991
ptdixon <pdixon@dna.ie>
2015-01-07 10:36:20 +0000

    Cleanup...

--- man/makepackage.sh
+++
@@ -1,4 +0,0 @@
-# run in parent of node-logentries folder
-
-mkdir -p node-logentries/dist
-tar -czf node-logentries/dist/node-logentries.tgz node-logentries/package.json node-logentries/README.md node-logentries/lib/logentries.js 
