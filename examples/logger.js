54feb5f4816d2e4d4ebf9d6a39d77a4fe3852286
Paul Dixon <paul.dixon@logentries.com>
2015-01-07 13:55:41 +0000

    Add end() to winston.js and a few comments..

--- examples/logger.js
+++ examples/logger.js
@@ -1,5 +1,7 @@
+// Test logentries log service. You must insert your logentries token below !
+
 var le=require('../lib/logentries.js'); // le=require('node-logentries')
-var logger = le.logger({ token: '1ed3e842-b5a1-4a8d-9783-0f266e7a6a4d' });
+var logger = le.logger({ token: 'YOUR_TOKEN' });
 
 logger.on('error',function(err){
   console.log('LOG ERROR: ', err);
--- examples/winston.js
+++ examples/winston.js
@@ -1,3 +1,5 @@
+// Test logentries log service as transport in winston
+
 var winston=require('winston'), le=require('../lib/logentries.js'); // le=require('node-logentries')
 
 var leToken='YOUR_LE_TOKEN', leLog=le.logger({token:leToken});
@@ -18,3 +20,7 @@ wl.log('info', 'INFO Howdy..');
 wl.log('warn', 'WARN Howdy..', ['what','about','arrays']);
 wl.log('error', 'ERROR Howdy %s %s', 'template', 'string', {a:1, b:2}, function(){ console.log('Why would I want to do this?') });
 wl.log('Whatever...'); // Should do nothing!
+
+leLog.end();
+
+console.log('Finished');
